c18e2016b9046872ed4592f73abb17a8e32d05d3
Eniola Samuel <theeniolasamuel@gmail.com>
2024-12-10 13:29:05 +0100

    continuation

---
+++ src/main/java/com/SkBHousing/skbhousingapp/controllers/BookingController.java
@@ -0,0 +1,27 @@
+package com.SkBHousing.skbhousingapp.controllers;
+
+import com.SkBHousing.skbhousingapp.data.models.Booking;
+import com.SkBHousing.skbhousingapp.data.models.ApartmentStatus;
+import com.SkBHousing.skbhousingapp.dtos.requests.BookApartmentRequest;
+import com.SkBHousing.skbhousingapp.dtos.responses.BookedApartmentResponse;
+import com.SkBHousing.skbhousingapp.services.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bookings")
+@RequiredArgsConstructor
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    // Endpoint to create a new booking
+    @PostMapping("/book")
+    public ResponseEntity<BookedApartmentResponse> bookApartment(@RequestBody BookApartmentRequest bookApartmentRequest) {
+        BookedApartmentResponse response = bookingService.userBookApartment(bookApartmentRequest);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
