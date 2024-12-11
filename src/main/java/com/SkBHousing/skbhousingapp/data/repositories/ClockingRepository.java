0cc256fa4313444fd5e267fbff00f0549b702ca4
Eniola Samuel <theeniolasamuel@gmail.com>
2024-12-11 14:37:08 +0100

    continuation

--- src/main/java/com/SkBHousing/skbhousingapp/data/repositories/ClockingRepository.java
+++ src/main/java/com/SkBHousing/skbhousingapp/data/repositories/ClockingRepository.java
@@ -1,8 +1,10 @@
 package com.SkBHousing.skbhousingapp.data.repositories;
 
 import com.SkBHousing.skbhousingapp.data.models.Clocking;
+import com.SkBHousing.skbhousingapp.data.models.Clocking_Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClockingRepository extends JpaRepository<Clocking, Long> {
 
+    Clocking findByPhoneNumberAndClockingStatus(String customerPhoneNumber, Clocking_Status clockingStatus);
 }
