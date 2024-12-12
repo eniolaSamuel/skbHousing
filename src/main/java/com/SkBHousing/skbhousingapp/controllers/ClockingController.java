47d324f1bf98959d77942dbeadab2b749568e895
Eniola Samuel <theeniolasamuel@gmail.com>
2024-12-12 13:50:45 +0100

    continuation

--- src/main/java/com/SkBHousing/skbhousingapp/controllers/ClockingController.java
+++ src/main/java/com/SkBHousing/skbhousingapp/controllers/ClockingController.java
@@ -1,4 +1,24 @@
 package com.SkBHousing.skbhousingapp.controllers;
 
+import com.SkBHousing.skbhousingapp.services.ClockingService;
+import com.SkBHousing.skbhousingapp.utils.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/clocking")
+@RequiredArgsConstructor
+
 public class ClockingController {
-}
+
+    private final ClockingService clockingService;
+    private static final Logger logger = LoggerFactory.getLogger(ClockingController.class);
+
+/**
+ * Endpoint for clocking in a user.
+ *
+ * @param userPhoneNumber Phone number of the user attempting to clock in.
+ * @return ApiResponse indicating the result of the clock-in attempt.
+ */
