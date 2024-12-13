8d860d15ff1fd3ea98fa3ee2a2fc94d58e32f7b0
Eniola Samuel <theeniolasamuel@gmail.com>
2024-12-13 12:31:01 +0100

    continuation

--- src/main/java/com/SkBHousing/skbhousingapp/utils/ApiResponse.java
+++ src/main/java/com/SkBHousing/skbhousingapp/utils/ApiResponse.java
@@ -8,12 +8,47 @@ import org.springframework.http.HttpStatus;
 @Builder
 @Getter
 @Setter
-
 public class ApiResponse {
     private Object data;
     private HttpStatus httpStatus;
     private int statusCode;
     private boolean isSuccessful;
 
+    // Factory method for success response
+    public static ApiResponse okResponse(String message, Object data) {
+        return ApiResponse.builder()
+                .data(data)
+                .httpStatus(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
+                .isSuccessful(true)
+                .build();
+    }
 
+    public static ApiResponse okResponse(String message) {
+        return ApiResponse.builder()
+                .data(message)
+                .httpStatus(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
+                .isSuccessful(true)
+                .build();
+    }
+
+    // Factory method for error response
+    public static ApiResponse errorResponse(String message) {
+        return ApiResponse.builder()
+                .data(message)
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .isSuccessful(false)
+                .build();
+    }
+
+    public static ApiResponse errorResponse(String message, Object data) {
+        return ApiResponse.builder()
+                .data(data)
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .isSuccessful(false)
+                .build();
+    }
 }
