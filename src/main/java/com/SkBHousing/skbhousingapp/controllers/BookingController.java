1292462d7da31486730e4409c4949033a62dab5e
Eniola Samuel <theeniolasamuel@gmail.com>
2024-12-18 10:05:54 +0100

    continuation

--- src/main/java/com/SkBHousing/skbhousingapp/controllers/BookingController.java
+++ src/main/java/com/SkBHousing/skbhousingapp/controllers/BookingController.java
@@ -55,7 +55,7 @@ public class BookingController {
         return ResponseEntity.ok(booking);
     }
 
-    //delete
+    //delete the all
     @DeleteMapping("/delete-all")
     public ResponseEntity<String> deleteAllBookings() {
         bookingService.deleteAll();
